96978acc3021367a4a14de530ad6677cb14ffef5
subash <subashbalaji.god@gmail.com>
2026-02-27 20:40:17 +0530

    Changes in schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,26 @@
-from pydantic import BaseModel, EmailStr, conint
+from pydantic import BaseModel, EmailStr, conint, ConfigDict
 from typing import Annotated
-
 from datetime import datetime
 
-class  Product(BaseModel):
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Product(BaseModel):
     name: str
     inventory: int
     price: int
+
 
 class ProductOut(BaseModel):
     id: int
@@ -15,33 +29,30 @@ class ProductOut(BaseModel):
     price: int
     owner_id: int
     owner: UserOut
-class User(BaseModel):
-    email: EmailStr
-    password: str
 
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    created_at: datetime
+    model_config = ConfigDict(from_attributes=True)
+
 
 class Login(BaseModel):
     email: EmailStr
     password: str
 
-class LoginOut(BaseModel): 
+
+class LoginOut(BaseModel):
     id: int
     email: EmailStr
     created_at: datetime
+
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class TokenData(BaseModel):
     id: int | None = None
+
 
 class Like(BaseModel):
     prod_id: int
     dir: Annotated[int, conint(ge=0, le=1)]
-class Config:
-    orm_mode = True
